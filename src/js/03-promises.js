3d2a2fd9b9ccca1f8aaf616591fc6fc11c6ae227
RenataPerduk <perdukrenata@gmail.com>
2021-08-17 09:59:04 +0300

    Правки

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,25 +2,22 @@ const btnRef = document.querySelector('write');
 btnRef.addEventListener('change', createPromise);
 
 function createPromise() {
-    return new Promise((2, 1500) {
+    return new Promise((2, 1500),
         setTimeout(() => {
-        const shouldResolve = Math.random() > 0.3;
+            const shouldResolve = Math.random() > 0.3;
 
-        if(shouldResolve) {
-            resolve(promise.write, delay);
-        } else {
-            reject(promise.write);
+            if (shouldResolve) {
+                resolve(promise.write, delay);
+            } else {
+                reject(promise.write);
+            }
+        }, delay),
+
+        logSuccess = (position, delay) => {
+            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        },
+
+        logError = (position, delay) => {
+            console.warn(`❌ Rejected promise ${position} in ${delay}ms`);
+        })
 }
-            }, delay);
-        });
-    };
-
-const logSuccess = (position, delay) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-};
-
-const logError = (position, delay) => {
-    console.warn(`❌ Rejected promise ${position} in ${delay}ms`);
-};
-
-
