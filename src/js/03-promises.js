3fc0815b62e2381ce589396d34e75b07ef6442fd
RenataPerduk <perdukrenata@gmail.com>
2021-08-18 19:02:07 +0300

    Правки

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,23 +1,97 @@
-const btnRef = document.querySelector('write');
-btnRef.addEventListener('change', createPromise);
+const searchForm = document.querySelector('form');
+searchForm.addEventListener('submit', createPromise);
 
 function createPromise() {
     return new Promise((2, 1500),
         setTimeout(() => {
             const shouldResolve = Math.random() > 0.3;
 
-            if (shouldResolve) {
-                resolve(promise.write, delay);
+            if (shouldResolve.searchForm.addEventListener) {
+                resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
             } else {
-                reject(promise.write);
+                reject(`❌ Rejected promise ${position} in ${delay}ms`);
             }
-        }, delay),
-
-        logSuccess = (position, delay) => {
-            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        },
-
-        logError = (position, delay) => {
-            console.warn(`❌ Rejected promise ${position} in ${delay}ms`);
-        })
+        }, delay))
 }
+
+
+        // logSuccess = (position, delay) => {
+        //     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        // },
+
+        // logError = (position, delay) => {
+        //     console.warn(`❌ Rejected promise ${position} in ${delay}ms`);
+        // })
+
+// const refs = {
+//     fetchCreatePromise: document.querySelector('delay')
+// };
+
+// refs.newCreatePromise.addEventListener('submit', createPromise);
+
+// function newCreatePromise(position, delay) {
+//     const shouldResolve = Math.random() > 0.3;
+//     refs.CreatePromise.innerHTML = markup;
+
+//     fetchCreatePromise(2, 1500)
+//         .then(resolve => {
+//             console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         })
+//         .catch(error => {
+//             console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//         })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// const delay = document.querySelector('delay')
+// const step = document.querySelector('step')
+// const amount = document.querySelector('amount')
+
+
+// delay.addEventListener('change', newDelay);
+// step.addEventListener('change', newStep);
+// amount.addEventListener('change', newAmount);
+// btnCreatePromise.addEventListener('submit', createPromise);
+
+
+
+// const createPromise = new createPromise((position, delay) => {
+
+//     const shouldResolve = Math.random() > 0.3;
+
+//     setTimeout(() => {
+//         if (shouldResolve) {
+//             resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         };
+//         reject(`❌ Rejected promise ${position} in ${delay}ms`);
+//     }, delay);
+// });
+// return promise;
+
+// btnCreatePromise.addEventListener('submit', createPromise)
+
+
+
+// function newCreatePromise({ delay, step, amount }) {
+//     refs.newCreatePromiseDelay.textContent = `${delay}`;
+//     refs.newCreatePromiseStep.textContent = `${step}`;
+//     refs.newCreatePromiseAmount.textContent = `${amount}`;
+
+//     createPromise(2, 1500)
+//     shouldResolve = Math.random() > 0.3
+//         .then(({ position, delay }) => {
+//             console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         })
+//         .catch(({ position, delay }) => {
+//             console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//         });
